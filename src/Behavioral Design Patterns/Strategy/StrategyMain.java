edff5e1197afe98eef11db88b24293d29b3e18a1
Janvi <janvi.singla2018@gmail.com>
2025-03-08 21:25:04 +0530

    Added some modules

---
+++ src/Behavioral Design Patterns/Strategy/StrategyMain.java
@@ -0,0 +1,5 @@
+public class StrategyMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Structural Design Patterns/Adapter/AdapterMain.java
@@ -0,0 +1,5 @@
+public class AdapterMain {
+    public static void main(String[] args) {
+
+    }
+}
