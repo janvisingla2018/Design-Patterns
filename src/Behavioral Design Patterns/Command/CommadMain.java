fe5d000292742b53899f77d1a3756f162b0b20b0
Janvi <janvi.singla2018@gmail.com>
2025-03-13 08:03:21 +0530

    Implements Command Design Pattern

---
+++ src/Behavioral Design Patterns/Command/CommadMain.java
@@ -0,0 +1,14 @@
+public class CommadMain {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Command turnOnLightCommand = new TurnOnLightCommand(light);
+        Command turnOffLightCommand = new TurnOffLightCommand(light);
+
+        RemoteControl remoteControl = new RemoteControl();
+        remoteControl.setCommand(turnOnLightCommand);
+        remoteControl.pressButton();
+
+        remoteControl.setCommand(turnOffLightCommand);
+        remoteControl.pressButton();
+    }
+}
